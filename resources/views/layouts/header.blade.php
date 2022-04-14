b0bd69a695e8afc24ff5e2d03a6ed3f3c3d569b1
Nicolas Olaya <naolay@misena.edu.co>
2022-04-13 22:13:04 -0500

    Creacion de la plantilla del header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atenea Software</title>
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="contenedor-total">
+        <header class="cabecera">
+
+            <div class="cabecera-detalles">
+                <div class="icono">
+                    <img src="{{ asset('img/qZlYb2O__400x400.jpg') }}">
+                </div>
+                
+                <div class="rectangulo-cabecera">
+                    <div class="titulo-usuario">
+                        <h2>Cesar clavijo</h2>
+                    </div>
+                    <div class="titulo-rol">
+                        <h2>Administrador</h2>
+                    </div>
+                    <div class="contenedor-botones">
+                        <a class="boton" href="">Ver perfil</a>
+                        <a class="boton" href="">Cerrar sesion</a>
+                    </div>         
+                </div>            
+            </div>
+        </header>
+
+            <div class="nav-bg">
+                <nav class="navegacion-principal">
+                    <!-- <nav class="botones-nav"> -->
+                        <a href="../../../generales/Home/Marco/Home.html"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#145682" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <polyline points="5 12 3 12 12 3 21 12 19 12" />
+                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+                        </svg></a>
+                    <!-- </nav> -->
+                    <p>|</p>
+                    <div class="botones-nav">
+                        <a href="#">Usuarios</a>
+                    </div>
+                    <p>|</p>
+                    <div class="botones-nav">
+                        <a href="#">Habitaciones</a>
+                    </div>
+                    <p>|</p>
+                    <div class="botones-nav">
+                        <a href="#">Clientes</a>
+                    </div>
+                    <p>|</p>
+                    <div class="botones-nav">
+                        <a href="#">Empleados</a>
+                    </div>
+                    <p>|</p>
+                    <div class="botones-nav">
+                        <a href="#">Servicios</a>
+                    </div>
+
+                </nav>
+            </div> 
+
+        <main class="principal">
+
+
+</body>
+</html>
