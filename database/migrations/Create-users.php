d5b1ffec2717b2bca8ae719159a8b187f1da5ce2
Juandres03 <jaalvarado342@misena.edu.co>
2022-04-28 10:52:23 -0500

    tablas relacionadas

--- database/migrations/Create-users.php
+++ database/migrations/Create-users.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             
+            
              
             
         });
