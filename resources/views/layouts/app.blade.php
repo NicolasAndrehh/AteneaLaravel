8d521b645c8dbec2451f32ec427bd1cccc334db3
Juandres0903 <juandresalvarado3@gmail.com>
2022-05-03 12:23:37 -0500

    actualizaciones de vistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,14 +98,7 @@
         <div class=" nav-bg   bg-dark   shadow-sm">
             <nav class="navegacion-principal">
                 <!-- <nav class="botones-nav"> -->
-                    <a href="../../../generales/Home/Marco/Home.html"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#145682" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <polyline points="5 12 3 12 12 3 21 12 19 12" />
-                        <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                        <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                    </svg></a>
-                <!-- </nav> -->
-                <p>|</p>
+                  
                 <div class="botones-nav">
                     <a href="{{ url('/usuario') }}">Usuarios</a>
                 </div>
